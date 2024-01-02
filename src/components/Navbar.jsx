76128b1e74dc0deee875abcf19eeff2b343d75f1
chamodperera <chamodperera87@gmail.com>
2024-01-03 00:13:31 +0530

    initial setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import styles from "../styles/Navbar.module.css";
+
+function Navbar() {
+  return (
+    <>
+      <div className="app">
+        <span className={styles.header_text}>HI</span>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
