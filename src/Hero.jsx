5e4129fee5203198384bce260d9a21e3be7a5352
Yehen Asuramuni <yehenaasuramuni@gmail.com>
2024-01-03 16:39:23 +0530

    Making required pages for hero section

---
+++ src/Hero.jsx
@@ -0,0 +1,18 @@
+import "./Hero.css"
+
+const Hero = () => {
+    return ( 
+        <div className="hero">
+            <div className="hero_section">
+                <div className="column1"></div>
+                <div className="column2">
+                    <span className="col2_head">Empowering your Business</span>
+                    <span className="colu2_body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+                    <button className="talk">Let's Talk</button>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Hero;
