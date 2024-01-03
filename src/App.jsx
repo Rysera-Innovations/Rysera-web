820e385f643d824fd966d00ee724b8ee96112493
Savinu Anusara <savinu0002@gmail.com>
2024-01-04 03:52:54 +0530

    Made the about us bottom part

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Aboutusb from "./components/Aboutusb";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <div className="App">
         <Navbar />
+        <Aboutusb/>
       </div>
     </>
   );
