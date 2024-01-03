a7dd7e0df8c06edd44470c6e11c1b86983885451
Yehen Asuramuni <yehenaasuramuni@gmail.com>
2024-01-03 21:02:17 +0530

    Made a non-renposive version of the hero section

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,13 +1,16 @@
 import "./Hero.css"
+import hero_pic from "./assets/hero_pic.png"
 
 const Hero = () => {
     return ( 
         <div className="hero">
             <div className="hero_section">
-                <div className="column1"></div>
+                <div className="column1">
+                    <img src={hero_pic} alt="Futuristic plant design" className="hero_pic"/>
+                </div>
                 <div className="column2">
-                    <span className="col2_head">Empowering your Business</span>
-                    <span className="colu2_body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+                    <div className="col2_head">Empowering your Business</div>
+                    <div className="col2_body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
                     <button className="talk">Let's Talk</button>
                 </div>
             </div>
