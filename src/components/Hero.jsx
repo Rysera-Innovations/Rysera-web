b6b06a44d08398f8800d995192872247cf4dba6f
Yehen Asuramuni <yehenaasuramuni@gmail.com>
2024-01-05 02:09:50 +0530

    needs only the scrolling background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,11 @@
-import styles from "../styles/HeroYehen.module.css"
-import styles2 from "../styles/Hero.module.css"
+import styles from "../styles/Hero.module.css"
 import hero_pic from "../assets/hero_pic.png"
 import {Link} from 'react-scroll';
 
 const Hero = () => {
     return ( 
         <div id='Hero' className={styles.hero}>
-            <span className={styles2.hero_background_1}>RYSERA INNOVATIONS</span>
+            <span className={styles.hero_background_1}>RYSERA INNOVATIONS</span>
             <div className={styles.hero_section}>
               <div className={styles.column1}>
                   <img src={hero_pic} alt="Futuristic plant design" className={styles.hero_pic}/>
@@ -17,7 +16,7 @@ const Hero = () => {
                   <Link to='ContactUs' smooth={true} duration={500}><button className={styles.talk}>Let's Talk</button></Link>
               </div>
             </div>
-            <span className={styles2.hero_background_2}>RYSERA INNOVATIONS</span>   
+            <span className={styles.hero_background_2}>RYSERA INNOVATIONS</span>   
         </div>
      );
 }
