94c07bff5c26dddd41e78a2210ecf11775fc8b3b
Sahan Cooray <sahantharuka2002@gmail.com>
2024-01-04 15:19:27 +0530

    added-navbar-display-none

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import ContactUs from "./components/ContactUs";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <div className="App">
+        <Navbar />
         <ContactUs />
         <Footer />
       </div>
