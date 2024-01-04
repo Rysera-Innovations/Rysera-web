eeccf1e48f569fa57eef07682e558529c9afad35
chamodperera <chamodperera87@gmail.com>
2024-01-04 09:27:18 +0530

    fixed responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
+import AboutUs from "./components/AboutUs";
 
 function App() {
   return (
     <>
       <div className="App">
-        <Navbar />
+        <AboutUs />
       </div>
     </>
   );
--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,24 +1,26 @@
 import styles from "../styles/AboutUs.module.css";
-import phone from "../assets/phone.svg"
+import phone from "../assets/phone.svg";
 
 const AboutUs = () => {
-    return ( 
-        <div className={styles.main}>
-                <div className={styles.left_side}>
-                        <div className={styles.title}>
-                            About US
-                        </div>
-                        <div className={styles.description}>            
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu ultrices vitae auctor eu. Sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus.
-                        </div>
-                    
-                </div>
-    
-                <div className={styles.right_side}>
-                    <img className={styles.phone} src={phone} />
-                </div>
+  return (
+    <div className={styles.main}>
+      <div className={styles.left_side}>
+        <div className={styles.content_container}>
+          <div className={styles.title}>About US</div>
+          <div className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu
+            ultrices vitae auctor eu. Sociis natoque penatibus et magnis dis
+            parturient montes nascetur ridiculus.
+          </div>
         </div>
-     );
-}
- 
+      </div>
+
+      <div className={styles.right_side}>
+        <img className={styles.phone} src={phone} />
+      </div>
+    </div>
+  );
+};
+
 export default AboutUs;
