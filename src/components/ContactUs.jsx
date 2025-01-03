26c74220ed362748d96c592c27a805bc8ca4278a
chamodperera <chamodperera87@gmail.com>
2025-01-03 14:48:52 +0530

    updated contact us

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -5,18 +5,23 @@ const ContactUs = () => {
   return (
     <div className={styles.container}>
       <div className={styles.title_container}>
-        <span>Let's talk about your Business</span>
+        <span>Let&apos;s talk about your Business</span>
       </div>
       <div className={styles.description_container}>
         <div className={styles.description}>
           <span>
-            Ready to Ryse? Let's talk. Drop us a message, and together, we'll
-            explore the possibilities that turn your dreams into reality.
+            Ready to Ryse? Let&apos;s talk. Drop us a message, and together,
+            we&apos;ll explore the possibilities that turn your dreams into
+            reality.
           </span>
         </div>
         <div className={styles.button_container}>
           <div className={styles.button}>
-            <button>
+            <button
+              onClick={() => {
+                window.open("https://wa.me/+94703359118", "_blank"); // Replace with the actual phone number
+              }}
+            >
               <span className={styles.button_text}>Contact Us</span>
               <ArrowRight className={styles.icon} color="#f7f7f7" />
             </button>
